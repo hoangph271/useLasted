155727a181a5772feafb8551bc9804f1f4b38a80
Phan Huy Hoang <hoangph271@gmail.com>
2022-09-09 00:25:44 +0700

    [Code] Split code (WIP)

---
+++ src/hooks/use-lasted.ts
@@ -0,0 +1,21 @@
+import formatDistance from "date-fns/formatDistance"
+import { useEffect, useState } from "react"
+import { D_DAYS } from "../utils"
+
+const getLasted = () => {
+  const startAt = new Date(D_DAYS[D_DAYS.length - 1])
+
+  return formatDistance(new Date(), startAt)
+}
+
+export const useLasted = () => {
+  const [lasted, setLasted] = useState(getLasted())
+
+  useEffect(() => {
+    setInterval(() => {
+      setLasted(getLasted())
+    }, 1000)
+  }, [])
+
+  return lasted
+}
