3e52ef0d0c1e8acbdc122f8987efcbb907f3afa5
Snêu <39024711+hoangph271@users.noreply.github.com>
2022-09-13 06:57:14 +0000

    [Fix] Remove old content

--- src/components/whys.tsx
+++ src/components/whys.tsx
@@ -1,9 +1,6 @@
 export const Whys = () => {
   const whys = [
     'Stay brown for 30 days, and THEN back to green...! :"}',
-    // 'No MS #Edge, so no #NSFW contents',
-    // 'No Safari, no saved files',
-    // 'No #Paids, no #FreeAcademics',
     'Really, it makes me feel so tired sometimes...!',
     '🍀'
   ]
